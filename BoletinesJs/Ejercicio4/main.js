8d9eeb25ba049a611d5ce099f2a45553ce6340fd
jousemarquez <mquezflores@gmail.com>
2023-01-31 20:42:21 +0100

    Inicio Ejercicio 4

---
+++ BoletinesJs/Ejercicio4/main.js
@@ -0,0 +1,10 @@
+/*
+Dado el array: const array = ["Joaquin", 1, ["Hola","Adios"], true]
+
+1) Recorrer el array e insertar dichos valores en el localstorage. El key de cada valor corresponderá a variable + i siendo i el indice en el que se encuentra el array.
+2) Recorrer de 0 a la longitud del array y hacer la peticion de variable + i del localstorage
+3) Crear un div por cada variable e insertar el valor de dicha variable en el div
+4) insertar estos divs en el body del html
+*/
+
+const array = ["Joaquin", 1, ["Hola","Adios"], true];
