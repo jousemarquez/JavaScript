1b3c731b38e15f83db98543666d103445b84b0ec
jousemarquez <mquezflores@gmail.com>
2023-02-04 16:25:10 +0100

    Práctica LocalStorage en Js

--- BoletinesJs/Ejercicio4/js/main.js
+++ BoletinesJs/Ejercicio4/js/main.js
@@ -7,26 +7,33 @@
 const array = ["Joaquin", 1, ["Hola","Adios"], true];
 
 // 1) Recorrer el array e insertar dichos valores en el localstorage
-let posicion = 0;
-while (posicion < array.length) {
-    localStorage.setItem(`variable${posicion}`, JSON.stringify(array[posicion]));
-    posicion++;
+
+for (item in array) {
+    localStorage.setItem("variable" + item, array[item]);
 }
 
 // 2) Recorrer de 0 a la longitud del array y hacer la petición de variable + i del localstorage
-posicion = 0;
-while (posicion < array.length) {
-    console.log(JSON.parse(localStorage.getItem(`variable${posicion}`)));
-    posicion++;
+
+for (item in array) {
+    console.log(localStorage.getItem("variable" + item));
 }
 
 // 3) Crear un div por cada variable e insertar el valor de dicha variable en el div
-const body = document.querySelector("BODY");
 
-posicion = 0;
-while (posicion < array.length) {
-    const div = document.createElement("DIV");
-    div.innerHTML = JSON.parse(localStorage.getItem(`variable${posicion}`));
-    body.appendChild(div);
-    posicion++;
-}
+let div1 = document.createElement("DIV");
+div1.textContent = localStorage.getItem("variable0");
+let div2 = document.createElement("DIV");
+div2.textContent = localStorage.getItem("variable1");
+let div3 = document.createElement("DIV");
+div3.textContent = localStorage.getItem("variable2");
+let div4 = document.createElement("DIV");
+div4.textContent = localStorage.getItem("variable3");
+
+//4) Insertar estos divs en el body del html.
+
+const body = document.querySelector("body");
+
+body.appendChild(div1);
+body.appendChild(div2);
+body.appendChild(div3);
+body.appendChild(div4);
