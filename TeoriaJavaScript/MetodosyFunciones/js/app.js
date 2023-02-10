63d073ca21602064e1b549b1d4c086ed8eff7d37
jousemarquez <mquezflores@gmail.com>
2023-02-10 15:16:35 +0100

    Actualización teoría

--- TeoriaJavaScript/MetodosyFunciones/js/app.js
+++ TeoriaJavaScript/MetodosyFunciones/js/app.js
@@ -25,20 +25,21 @@ function saludarNombre(nombre) {
 }
 
 //Function Expression (Se lee en la etapa de ejecucion)
-const saludar2 = function() {
+const saludar2 = function () {
     console.log("Hello");
 }
 
-const saludar2Nombre = function(nombre) {
+const saludar2Nombre = function (nombre) {
     console.log("Hello", nombre);
 }
 
 //Arrow Function (Se lee en la etapa de ejecucion)
 const saludar3 = () => console.log("Hello");
 const saludar3Nombre = (nombre) => console.log("Hello", nombre);
-const saludar3Nombre2 = (nombre) => {   nombre += ".";
-                                        console.log("Hello", nombre);
-                                    }
+const saludar3Nombre2 = (nombre) => {
+    nombre += ".";
+    console.log("Hello", nombre);
+}
 
 //Ejecución de funciones anteriores
 const profe = "Joaquin"
@@ -55,59 +56,60 @@ const numero = parseInt("1"); //Funcion (o metodo estatico) se accede con su pro
 const letra = "Hello".charAt(0); //Metodo que se accede mediante el objeto "Hello"
 
 //Valores por defecto
-function imprimirSuma(n1=0, n2 = 0) {
-    console.log(n1+n2);
+function imprimirSuma(n1 = 0, n2 = 0) {
+    console.log(n1 + n2);
 }
 
 imprimirSuma();
 imprimirSuma(1);
-imprimirSuma(1,2);
+imprimirSuma(1, 2);
 
 //Comunicacion entre funciones
 function imprimirArray(array) {
     let str = "";
-    for(let elem of array) {
+    for (let elem of array) {
         str += elem + " ";
     }
     console.log(str);
 }
 
 function imprimirArray2(array) {
-    for(let fila of array) {
+    for (let fila of array) {
         imprimirArray(fila);
     }
 }
 
-imprimirArray2([[1,2,3], [4,5,6], [7,8,9]]);
+imprimirArray2([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
 
 //Funciones que devuelven valores
-function sumar(n1=0, n2 = 0) {
-    return n1+n2;
+function sumar(n1 = 0, n2 = 0) {
+    return n1 + n2;
 }
 
 const resultado = sumar();
 const resultado2 = sumar(1);
-const resultado3 = sumar(1,2);
+const resultado3 = sumar(1, 2);
 console.log(resultado);
 console.log(resultado2);
 console.log(resultado3);
 
-const resta = (n1=0, n2=0) => n1-n2; //Si es una arrow function de una linea no hace falta return
+const resta = (n1 = 0, n2 = 0) => n1 - n2; //Si es una arrow function de una linea no hace falta return
 
 const resultado4 = resta();
 const resultado5 = resta(1);
-const resultado6 = resta(2,1);
+const resultado6 = resta(2, 1);
 console.log(resultado4);
 console.log(resultado5);
 console.log(resultado6);
 
-const mult = (n1=1, n2=1) => {  console.log("Vamos a multiplicar");
-                                return n1*n2; //Con mas lineas si hace falta return
-                            }
+const mult = (n1 = 1, n2 = 1) => {
+    console.log("Vamos a multiplicar");
+    return n1 * n2; //Con mas lineas si hace falta return
+}
 
 const resultado7 = mult();
 const resultado8 = mult(2);
-const resultado9 = mult(2,2);
+const resultado9 = mult(2, 2);
 console.log(resultado7);
 console.log(resultado8);
 console.log(resultado9);
@@ -118,7 +120,7 @@ console.log(resultado9);
 document.addEventListener('DOMContentLoaded', () => console.log("HTML CARGADO"));
 
 //eventos con el mouse
-const mainButton = document.querySelector("#mainBttn");  
+const mainButton = document.querySelector("#mainBttn");
 
 mainButton.addEventListener("click", () => console.log("Has hecho click"));
 mainButton.addEventListener("mouseenter", () => console.log("Has puesto el raton sobre el boton"));
@@ -128,7 +130,7 @@ mainButton.addEventListener("mouseup", () => console.log("Has dejado de pulsar e
 mainButton.addEventListener("dblclick", () => console.log("Has hecho doble click"));
 
 //Eventos sobre los inputs
-const subjectInput = document.querySelector("#subject");  
+const subjectInput = document.querySelector("#subject");
 
 subjectInput.addEventListener("keydown", () => console.log("Has presionado una tecla"));
 subjectInput.addEventListener("keyup", () => console.log("Has dejado de presionar una tecla"));
@@ -166,7 +168,7 @@ function sumarEnteros(a, b) {
     console.log(resultado);
 }
 
-formulario.addEventListener("submit", () => sumarEnteros(1,2));
+formulario.addEventListener("submit", () => sumarEnteros(1, 2));
 
 //Eventos al hacer scroll
 window.addEventListener("scroll", () => {
